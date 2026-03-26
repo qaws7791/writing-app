2a9d3a2a978cdb30f77e27a5b868e97fe6a7727a
qaws7791 <qaws7791@gmail.com>
2026-03-26 15:33:44 +0900

    QueryClient의 기본 옵션 설정 추가

--- apps/web/src/app/(protected)/providers.tsx
+++ apps/web/src/app/(protected)/providers.tsx
@@ -3,7 +3,28 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useMemo } from "react"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = useMemo(() => new QueryClient(), [])
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 0,
+            gcTime: 10 * 60 * 1000,
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: true,
+            refetchOnMount: true,
+            structuralSharing: true,
+            networkMode: "online",
+          },
+          mutations: {
+            retry: 0,
+            networkMode: "online",
+          },
+        },
+      }),
+    []
+  )
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
