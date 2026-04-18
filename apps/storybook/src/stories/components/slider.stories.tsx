fa77f2a81daf88528bdf243ce89a1398068cb862
qaws7791 <qaws7791@gmail.com>
2026-04-19 06:54:39 +0900

    slider가 안보이는 문제를 해결하기 위해 컨테이너 요소 추가

--- apps/storybook/src/stories/components/slider.stories.tsx
+++ apps/storybook/src/stories/components/slider.stories.tsx
@@ -18,18 +18,24 @@ type Story = StoryObj<typeof Slider>
 
 export const Default: Story = {
   render: () => (
-    <Slider defaultValue={[50]} max={100} step={1} className="w-[350px]" />
+    <div className="w-80 p-8">
+      <Slider defaultValue={[50]} max={100} step={1} />
+    </div>
   ),
 }
 
 export const Range: Story = {
   render: () => (
-    <Slider defaultValue={[25, 75]} max={100} step={1} className="w-[350px]" />
+    <div className="w-80 p-8">
+      <Slider defaultValue={[25, 75]} max={100} step={1} />
+    </div>
   ),
 }
 
 export const Steps: Story = {
   render: () => (
-    <Slider defaultValue={[50]} max={100} step={10} className="w-[350px]" />
+    <div className="w-80 p-8">
+      <Slider defaultValue={[50]} max={100} step={10} />
+    </div>
   ),
 }
