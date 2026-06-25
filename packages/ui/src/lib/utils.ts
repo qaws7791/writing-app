89b4aa0fa17dc8cc94f81371c972aaaceca00ca6
qaws7791 <qaws7791@gmail.com>
2026-06-26 02:26:06 +0900

    tailwind-merge 설정을 확장하여 커스텀 폰트 크기 클래스의 충돌 방지
    - packages/ui/src/lib/utils.ts 파일의 cn 함수에 extendTailwindMerge 적용
    
    - text-body-lg 등의 커스텀 폰트 크기 클래스가 text-action-primary-fg 같은 글자색 클래스를 지우는 문제 해결

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,6 +1,29 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { extendTailwindMerge } from "tailwind-merge"
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        "text-display-lg",
+        "text-display-md",
+        "text-heading-xl",
+        "text-heading-lg",
+        "text-heading-md",
+        "text-heading-sm",
+        "text-title-lg",
+        "text-title-md",
+        "text-body-lg",
+        "text-body-md",
+        "text-body-sm",
+        "text-label-md",
+        "text-label-sm",
+        "text-caption",
+      ],
+    },
+  },
+})
 
 export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs))
+  return customTwMerge(clsx(inputs))
 }
